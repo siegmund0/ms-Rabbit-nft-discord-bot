681ef05219cac10caf28d3f2fe808cc06a32aaa0
Ignacio Gonzalez <nachogonza123@gmail.com>
2021-10-26 02:33:37 -0300

    Modifico comando TEST

--- src/commands/test-command.ts
+++ src/commands/test-command.ts
@@ -3,6 +3,7 @@ import { ApplicationCommandData, CommandInteraction } from 'discord.js';
 import { EventData } from '../models/internal-models';
 import { Lang } from '../services';
 import { MessageUtils } from '../utils';
+import { CryptoMinesEthernal } from '../webServices/CryptominersEternal';
 import { Command } from './command';
 
 export class TestCommand implements Command {
@@ -15,6 +16,19 @@ export class TestCommand implements Command {
     public requirePerms = [];
 
     public async execute(intr: CommandInteraction, data: EventData): Promise<void> {
-        await MessageUtils.sendIntr(intr, Lang.getEmbed('displayEmbeds.test', data.lang()));
+        let response = await CryptoMinesEthernal();
+        await MessageUtils.sendIntr(
+            intr,
+            Lang.getEmbed('displayEmbeds.test', data.lang(), {
+                TOKEN: response.symbol,
+                TOKEN_NAME: response.name,
+                PLATAFORM: response.asset_platform_id,
+                BSCSCAN_PAGE: response.links.blockchain_site[0],
+                CONTRACT_ADDRESS: response.contract_address,
+                USD_PRICE: response.market_data.current_price.usd,
+                BNB_PRICE: response.market_data.current_price.bnb,
+                TOTAL_SUPPLY: response.market_data.total_supply,
+                TRADE_URL: response.tickers[0].trade_url
+            }));
     }
 }
